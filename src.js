ca4be83b7b6e32c12aef4b003a125e9c8e738edc
jlawless <john.steven.lawless@cern.ch>
2025-01-30 21:20:38 +0100

    Trial of random garbage

--- src.js
+++ src.js
@@ -1,24 +1,8 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-// Example();
-const e = react_1.default.createElement;
-class LikeButton extends react_1.default.Component {
-    constructor(props) {
-        super(props);
-        this.state = { liked: false };
-    }
-    render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-        return e('button', { onClick: () => this.setState({ liked: true }) }, 'Like');
-    }
-}
+const e = App();
+
+
+
+
 const domContainer = document.querySelector('#like_button_container');
-//@ts-ignore
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(eLikeButton);
